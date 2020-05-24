94d5545c8ba829cd101950ca57b706cd17d3b212
Milinda Bandara <dev.milinda@gmail.com>
2020-05-24 23:32:10 +0530

    modified css for naviagtion selection

--- src/ContentDiv/ConDevice/DeviceManager.js
+++ src/ContentDiv/ConDevice/DeviceManager.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Accodian from './../../UtilComponent/Accodian/Accodian'
+import './DeviceManager.css'
 
 export default function DeviceManager() {
     return (
         <div>
+            <div id= "header"><h1>Device Manager</h1></div>
             <Accodian  title="Electra Nodes" content="Sapumal Bandara" />
-            {/* <Accodian  title="Electra Device" content="Milinda Switch Bandara" /> */}
+            <Accodian  title="Electra Device" content="Milinda Switch Bandara" />
         </div>
     )
 }
