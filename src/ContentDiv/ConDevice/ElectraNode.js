e70f61bff43927484570123ebb487957e95af96a
Milinda Bandara <dev.milinda@gmail.com>
2020-06-05 23:15:48 +0530

    node selected properies update box implementation

--- src/ContentDiv/ConDevice/ElectraNode.js
+++ src/ContentDiv/ConDevice/ElectraNode.js
@@ -5,30 +5,37 @@ import './DeviceManagerNode.css'
 
 export default class ElectraNode extends Component {
 
+    state = {
+        projectNodesList: [],
+
+        isSelectedNode: "",
+        selectedNode: {}
+    }
+
     constructor() {
         super();
         this.loadProjectNodes();
     }
 
-    state = {
-        selectedNode: [],
-        projectNodesList: [],
 
-        isSelectedNode: ""
+    componentDidUpdate(prevProps, prevState) {
+        let varProject = this.props === undefined ? "" : this.props.project;
+
+        let temp = (!(varProject.nodesList === "" || varProject.nodesList === undefined)) && (prevProps.project !== this.props.project) ? this.setState({ projectNodesList: varProject.nodesList }) : ""
 
     }
 
     loadProjectNodes() {
 
-        let node1 = new ElectraModelNode(1, "192.168.1.7", "5000", "micro");
-        let node2 = new ElectraModelNode(3, "192.168.1.4", "5000", "nano");
-        let node3 = new ElectraModelNode(2, "192.168.1.100", "5000", "micro");
-
         let nodeList = this.state.projectNodesList;
-        nodeList.push(node1);
-        nodeList.push(node2);
-        nodeList.push(node3);
 
+        // let node1 = new ElectraModelNode(1, "192.168.1.7", "5000", "micro");
+        // let node2 = new ElectraModelNode(3, "192.168.1.4", "5000", "nano");
+        // let node3 = new ElectraModelNode(2, "192.168.1.100", "5000", "micro");
+
+        // nodeList.push(node1);
+        // nodeList.push(node2);
+        // nodeList.push(node3);
 
         this.setState({
             projectNodesList: nodeList
@@ -38,10 +45,16 @@ export default class ElectraNode extends Component {
 
     renderToModify(id) {
 
-        this.setState({
-            isSelectedNode: id
+        this.state.projectNodesList.forEach(scanNode => {
+            if (scanNode.id === id) {
+                this.setState({
+                    selectedNode: scanNode,
+                    isSelectedNode: id
+                })
+                return;
+            }
         })
-        console.log(id + " - " + this.state.isSelectedNode);
+        console.log(id + " - " + this.state.selectedNode.ip);
 
     }
 
@@ -83,23 +96,26 @@ export default class ElectraNode extends Component {
     }
 
     renderDetailTab() {
-        return(<div style={{ paddingTop: "21px" }}>
+        return (<div style={{ paddingTop: "21px" }}>
             <div className="nodeRow">
                 <div className="keyName keyName-dm">Project</div>
                 <input className="input-dm" value={this.props.project.name === "" || this.props.project.name === undefined ? "" : this.props.project.name} disabled ></input>
             </div>
             <div className="nodeRow">
+                <div className="keyName keyName-dm">Node Id :</div>
+                <input className="input-dm" onChange={this.OnChangeListnerText.bind(this, "id")} value={this.state.selectedNode.id === "" || this.state.selectedNode.id === undefined ? "" : this.state.selectedNode.id}></input>
+            </div>
+            <div className="nodeRow">
                 <div className="keyName keyName-dm">Node Type :</div>
-                <input className="input-dm"></input>
+                <input className="input-dm" onChange={this.OnChangeListnerText.bind(this, "type")} value={this.state.selectedNode.type === "" || this.state.selectedNode.type === undefined ? "" : this.state.selectedNode.type}></input>
             </div>
             <div className="nodeRow">
                 <div className="keyName keyName-dm">IP Address :</div>
-                <input className="input-dm"></input>
+                <input className="input-dm" onChange={this.OnChangeListnerText.bind(this, "ip")} value={this.state.selectedNode.ip === "" || this.state.selectedNode.ip === undefined ? "" : this.state.selectedNode.ip}></input>
             </div>
             <div className="nodeRow">
                 <div className="keyName keyName-dm">Port :</div>
-
-                <input className="input-dm"></input>
+                <input className="input-dm" onChange={this.OnChangeListnerText.bind(this, "port")} value={this.state.selectedNode.port === "" || this.state.selectedNode.port === undefined ? "" : this.state.selectedNode.port}></input>
             </div>
             <div className="nodeRow" style={{ padding: "7px 15px" }}>
                 <button className="btn-dm button-card">Add</button>
@@ -108,6 +124,21 @@ export default class ElectraNode extends Component {
                 <button className="btn-dm button-card">Cancel</button>
             </div>
         </div>);
+    }
+
+    OnChangeListnerText(attrib, event) {
+        event.persist();
+
+        if (attrib === 'id') {
+            this.setState(prevState => ({ selectedNode: { ...prevState.selectedNode, id: event.target.value } }));
+        } else if (attrib === 'type') {
+            this.setState(prevState => ({ selectedNode: { ...prevState.selectedNode, type: event.target.value } }));
+        } else if (attrib === 'ip') {
+            this.setState(prevState => ({ selectedNode: { ...prevState.selectedNode, ip: event.target.value } }));
+        } else if (attrib === 'port') {
+            this.setState(prevState => ({ selectedNode: { ...prevState.selectedNode, port: event.target.value } }));
+        }
+
     }
 
     render() {
